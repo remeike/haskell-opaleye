dafa461a1cc9be7b4561d5e55c35d59b6e56b494
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-06-14 19:52:37 +0100

    Try to avoid deprecation problems with Constant

--- src/Opaleye/ToFields.hs
+++ src/Opaleye/ToFields.hs
@@ -1,6 +1,12 @@
-module Opaleye.ToFields (C.toFields, C.ToFields, module Opaleye.ToFields) where
+{-# LANGUAGE FlexibleContexts #-}
+
+module Opaleye.ToFields (C.ToFields, module Opaleye.ToFields) where
 
 import qualified Opaleye.Constant as C
+import qualified Data.Profunctor.Product.Default as D
 
 toFieldsExplicit :: C.ToFields haskells fields -> haskells -> fields
 toFieldsExplicit = C.constantExplicit
+
+toFields :: D.Default C.ToFields haskells fields => haskells -> fields
+toFields = C.toFields
